9b5790a86973b11a01f5a60a334623b71742b5d9
JonGMS <joaoggabriell@gmail.com>
2026-08-10 23:14:08 -0300

    feat: Montando ServicoNota na aplicação

---
+++ Aplicacao_J.iary/ModuloNota/ServicoNota.cs
@@ -0,0 +1,33 @@
+﻿using Dominio_J.iary.Compartilhado;
+using Dominio_J.iary.ModuloNota;
+using FluentResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacao_J.iary.ModuloNota
+{
+    public class ServicoNota
+    {
+        private IContextoPersistencia ContextoPersistencia;
+        public ServicoNota(IContextoPersistencia contextoPersistencia, IRepositorioNota repositorioNota)
+        {
+            
+        }
+
+        public Result<Nota> Inserir(Nota nota)
+        {
+            try
+            {
+
+                return Result.Ok(nota);
+            }
+            catch (Exception ex)
+            {
+                return Result.Fail(ex.Message);
+            }
+        }
+    }
+}
