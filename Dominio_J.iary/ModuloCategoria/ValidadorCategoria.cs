ba1fcb7952506912a1f4d622836e80641a531fa0
JonGMS <joaoggabriell@gmail.com>
2026-08-11 21:54:56 -0300

    feat: Validador de Categorias

---
+++ Dominio_J.iary/ModuloCategoria/ValidadorCategoria.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio_J.iary.ModuloCategoria
+{
+    public class ValidadorCategoria : AbstractValidator<Categoria>
+    {
+        public ValidadorCategoria()
+        {
+            RuleFor(x => x.Nome)
+                .NotEmpty().WithMessage("O campo 'NOME' é obrigatório!")
+                .NotNull().WithMessage("O campo 'NOME' é obrigatório!")
+                .MinimumLength(3).WithMessage("O campo 'NOME' deve ter no mínimo 3 (três) caracteres!");
+
+            RuleFor(x => x.Cor)
+              .NotEmpty().WithMessage("O campo 'COR' é obrigatório!")
+              .NotNull().WithMessage("O campo 'COR' é obrigatório!");
+
+        }
+    }
+}
