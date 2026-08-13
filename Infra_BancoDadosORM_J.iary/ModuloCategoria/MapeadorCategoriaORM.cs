3ed016772b93f4506dbae7c989370958483c9a97
JonGMS <joaoggabriell@gmail.com>
2026-08-12 21:44:52 -0300

    feat: MapeadorORM de Categoria finalizado

---
+++ Infra_BancoDadosORM_J.iary/ModuloCategoria/MapeadorCategoriaORM.cs
@@ -0,0 +1,30 @@
+﻿using Dominio_J.iary.ModuloCategoria;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra_BancoDadosORM_J.iary.ModuloCategoria
+{
+    public class MapeadorCategoriaORM : IEntityTypeConfiguration<Categoria>
+    {
+        public void Configure(EntityTypeBuilder<Categoria> builder)
+        {
+            builder.ToTable("TB_CATEGORIA");
+            builder.Property(x => x.Nome)
+                .HasColumnName("NOME");
+            builder.Property(x => x.Cor)
+                .HasColumnName("COR");
+            builder.Property(x =>  x.UsuarioId)
+                .HasColumnName("ID_USUARIO")
+                .HasColumnType("uniqueidentifier")
+                .IsRequired();
+            builder.HasOne(x => x.Usuario)
+                .WithMany()
+                .HasForeignKey(x => x.UsuarioId);
+        }
+    }
+}
