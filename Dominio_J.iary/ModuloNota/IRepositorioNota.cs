f87a00734b3598bf0904bb4af223cb478521bd7d
JonGMS <joaoggabriell@gmail.com>
2026-08-14 22:29:32 -0300

    feat(nota): criar arquivo IRepositorioNota para repositório da Infra

---
+++ Dominio_J.iary/ModuloNota/IRepositorioNota.cs
@@ -0,0 +1,14 @@
+﻿using Dominio_J.iary.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio_J.iary.ModuloNota
+{
+    public interface IRepositorioNota : IRepositorioBase<Nota>
+    {
+
+    }
+}
