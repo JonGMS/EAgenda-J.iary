cc3754183b1a1760a680bf9f765326b9a2fc7692
JonGMS <joaoggabriell@gmail.com>
2026-08-20 23:17:31 -0300

    feat(nota): Validador nota

---
+++ Dominio_J.iary/ModuloNota/ValidadorNota.cs
@@ -0,0 +1,25 @@
+﻿using Dominio_J.iary.ModuloTarefa;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio_J.iary.ModuloNota
+{
+    public class ValidadorNota : AbstractValidator<Nota>
+    {
+        public ValidadorNota()
+        {
+            RuleFor(x => x.Titulo).NotEmpty().WithMessage("O campo 'TITULO' é obrigatório")
+                .NotNull().WithMessage("O campo 'TITULO' é obrigatório")
+                .MinimumLength(3).WithMessage("O campo 'TITULO' deve ter o mínimo 3 (três) caracteres!");
+            RuleFor(x => x.Categoria).NotEmpty().WithMessage("O campo 'CATEGORIA' é obrigatório")
+                .NotNull().WithMessage("O campo 'CATEGORIA' é obrigatório");
+            RuleFor(x => x.Armazenamento).NotEmpty().WithMessage("O campo 'ARMAZENAMENTO' é obrigatório!")
+               .NotNull().WithMessage("O campo 'ARMAZENAMENTO' é obrigatório!")
+               .Must(p => p == 'P' || p == 'C').WithMessage("O campo 'Prioridade' deve ser PADRÃO ou COFRE!");
+        }
+    }
+}
