b706a7b41d603a2ece0571b2cfa53ebafa887d07
JonGMS <joaoggabriell@gmail.com>
2026-08-26 23:06:33 -0300

    feat(cofre): Cria o objeto Cofre

---
+++ Dominio_J.iary/ModuloCofre/Cofre.cs
@@ -0,0 +1,28 @@
+﻿using Dominio_J.iary.Compartilhado;
+using Dominio_J.iary.ModuloUsuario;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio_J.iary.ModuloCofre
+{
+    public class Cofre : EntidadeBase<Cofre>
+    {
+        public Usuario Usuario { get; set; }
+        public Guid UsuarioId { get; set; }
+
+        public string Senha { get; set; }
+
+        public Cofre(Usuario usuario, string password)
+        {
+            UsuarioId = usuario.Id;
+            Senha = password;
+        }
+        public Cofre()
+        {
+            
+        }
+    }
+}
