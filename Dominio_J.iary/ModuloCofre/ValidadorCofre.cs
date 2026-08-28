452b2c23b739e548826335a9e9f0b602ffb8d3b6
JonGMS <joaoggabriell@gmail.com>
2026-08-27 21:40:56 -0300

    feat(cofre): Cria o validador de cofre, para a senha não ser nula

---
+++ Dominio_J.iary/ModuloCofre/ValidadorCofre.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio_J.iary.ModuloCofre
+{
+    public class ValidadorCofre : AbstractValidator<Cofre>
+    {
+        public ValidadorCofre()
+        {
+            RuleFor(x => x.Senha).NotEmpty().WithMessage("O campo 'SENHA' é obrigatório")
+            .NotNull().WithMessage("O campo 'SENHA' é obrigatório")
+            .MinimumLength(8).WithMessage("O campo 'SENHA' deve ter o mínimo 8 (oito) caracteres!");
+        }
+    }
+}
