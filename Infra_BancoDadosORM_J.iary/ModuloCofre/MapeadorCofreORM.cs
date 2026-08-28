c70b2c0755faed47f023db0faca4ffd1c88ef826
JonGMS <joaoggabriell@gmail.com>
2026-08-27 21:35:36 -0300

    feat(cofre): Cria o mapeador para Cofre

---
+++ Infra_BancoDadosORM_J.iary/ModuloCofre/MapeadorCofreORM.cs
@@ -0,0 +1,38 @@
+﻿using Dominio_J.iary.ModuloCofre;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra_BancoDadosORM_J.iary.ModuloCofre
+{
+    public class MapeadorCofreORM : IEntityTypeConfiguration<Cofre>
+    {
+        public void Configure(EntityTypeBuilder<Cofre> builder)
+        {
+            builder.ToTable("TB_COFRE");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .HasColumnName("ID");
+
+            builder.Property(x => x.UsuarioId)
+                .HasColumnName("ID_USUARIO")
+                .HasColumnType("uniqueidentifier")
+                .IsRequired();
+
+            builder.Property(x => x.Senha)
+                .HasColumnName("SENHA")
+                .HasColumnType("varchar(300)")
+                .IsRequired();
+
+            builder.HasOne(x => x.Usuario)
+                .WithOne()
+                .HasForeignKey<Cofre>(x => x.UsuarioId);
+        }
+    }
+}
