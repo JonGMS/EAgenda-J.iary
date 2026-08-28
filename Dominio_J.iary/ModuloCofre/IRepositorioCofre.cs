a7721638396923ee0b8a6a1344ddbd7390fa5700
JonGMS <joaoggabriell@gmail.com>
2026-08-27 21:44:52 -0300

    feat(cofre): cria o interface de Repositorio cofre

---
+++ Dominio_J.iary/ModuloCofre/IRepositorioCofre.cs
@@ -0,0 +1,15 @@
+﻿using Dominio_J.iary.Compartilhado;
+using Dominio_J.iary.ModuloUsuario;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio_J.iary.ModuloCofre
+{
+    public interface IRepositorioCofre : IRepositorioBase<Cofre>
+    {
+        bool VerificarCofre(Usuario usuario);
+    }
+}
