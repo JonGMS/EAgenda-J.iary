af1936c9cb8d9deec7ec8773422f79db566a4508
JonGMS <joaoggabriell@gmail.com>
2026-09-01 13:13:16 -0300

    refactor(service-locator): Alteração de interface

--- Apresentacao_J.iary/Compartilhado/ServiceLocator/IServiceLocator.cs
+++ Apresentacao_J.iary/Compartilhado/ServiceLocator/IServiceLocator.cs
@@ -10,7 +10,7 @@ namespace Apresentacao_J.iary.Compartilhado.ServiceLocator
     public interface IServiceLocator
     {
         T Get<T>() where T : ControladorBase;
-        void ExecutarInsercao(string nomeControlador);
+        void ExecutarSubMenu(string nomeControlador);
         void ArmazenarCofre(Cofre cofre);
         bool ConferirCofre();
     }
