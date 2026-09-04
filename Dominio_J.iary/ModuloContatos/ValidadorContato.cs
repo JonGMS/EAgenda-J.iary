045b884dbf80f6266a4723ea28d6eaf094bea411
JonGMS <joaoggabriell@gmail.com>
2026-09-04 14:11:31 -0300

    feat(contato): Validador contato

---
+++ Dominio_J.iary/ModuloContatos/ValidadorContato.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio_J.iary.ModuloContatos
+{
+    public class ValidadorContato : AbstractValidator<Contato>
+    {
+        public ValidadorContato()
+        {
+            RuleFor(x => x.Nome)
+                .NotEmpty().WithMessage("O campo 'NOME' é obrigatório!")
+                .NotNull().WithMessage("O campo 'NOME' é obrigatório!")
+                .MinimumLength(3).WithMessage("O campo 'NOME' deve ter no mínimo 3 (três) caracteres!");
+        }
+    }
+}
